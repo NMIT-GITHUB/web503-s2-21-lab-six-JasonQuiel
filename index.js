6ec75d1a97a8739229b6be1fdc6fb6a41351e682
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-09-29 14:30:23 +1300

    dont know what is going on

---
+++ index.js
@@ -0,0 +1,32 @@
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyD7r2Pg8VRvY6vqA8LHCR47ueV_uRHHuJs",
+
+    authDomain: "weblab6-c89cb.firebaseapp.com",
+
+    projectId: "weblab6-c89cb",
+
+    storageBucket: "weblab6-c89cb.appspot.com",
+
+    messagingSenderId: "416042648181",
+
+    appId: "1:416042648181:web:df68f54f9b050f824aee3c"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
